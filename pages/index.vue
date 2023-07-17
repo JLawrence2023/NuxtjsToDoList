d9a855c1b88a7acb8c05c20f4b1af2d26c5b9f78
Jose Lawrence Aquino <lawrencewebon@gmail.com>
2023-07-17 11:26:56 +0800

    Fix vue css code

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
             >
               <div class="assignment-text">
                 <div class="plus">➕</div>
-                <div class="add-assignment">課題の追加...</div>
+                <div class="add-issue">課題の追加...</div>
               </div>
 
               <div v-for="item in getList(1)" :key="item.id">
@@ -37,7 +37,7 @@
             >
               <div class="assignment-text">
                 <div class="plus">➕</div>
-                <div class="add-assignment">課題の追加...</div>
+                <div class="add-issue">課題の追加...</div>
               </div>
               <div v-for="item in getList(2)" :key="item.id">
                 <CardComponent
@@ -59,7 +59,7 @@
             >
               <div class="assignment-text">
                 <div class="plus">➕</div>
-                <div class="add-assignment">課題の追加...</div>
+                <div class="add-issue">課題の追加...</div>
               </div>
 
               <div v-for="item in getList(3)" :key="item.id">
@@ -82,7 +82,7 @@
             >
               <div class="assignment-text">
                 <div class="plus">➕</div>
-                <div class="add-assignment">課題の追加...</div>
+                <div class="add-issue">課題の追加...</div>
               </div>
 
               <div v-for="item in getList(4)" :key="item.id">
@@ -213,72 +213,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.task-list {
-  display: flex;
-  align-items: center;
-  width: 100%;
-}
-/* .todo {
-  margin-right: 40px;
-} */
-.add-assignment {
-  display: flex;
-  width: 70px;
-  height: 20px;
-  flex-direction: column;
-  justify-content: center;
-  flex-shrink: 0;
-  color: #02836b;
-  font-family: Inter;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-}
-.plus {
-  display: flex;
-  width: 13px;
-  height: 12px;
-  margin-left: 3px;
-  flex-direction: column;
-  justify-content: center;
-  flex-shrink: 0;
-  color: #808080;
-  text-align: center;
-  font-family: Inter;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  margin-right: 5px;
-}
-.assignment-text {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-}
-.rectangle {
-  width: 100%;
-  height: 574px;
-  background: white;
-  border-radius: 3px;
-  padding: 10px;
-  overflow-x: hidden;
-  overflow-y: auto;
-  min-width: 250px;
-  white-space: nowrap;
-}
-.container {
-  width: 100%;
-  overflow-x: auto;
-  white-space: nowrap;
-}
-.inner-container {
-  padding: 41px;
-  padding-top: 58px;
-}
-.content {
-  display: inline-block;
-}
-</style>
+<style scoped></style>
